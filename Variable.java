350722996d03f5e38c9b220cd5e1a78a727073f6
Hemavathi23-jm <acharyahema230@gmail.com>
2026-07-08 20:02:37 +0530

    Variable declariation and initialization and printing them

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        // Variable declaration and initialization
+        int age = 23;
+        float height = 5.9f;
+        long population = 78000000L;
+        char letter =  'H';
+        String name =  "Hema";
+
+        //Printing statements that prints values of the variables
+        System.out.println("Age : " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Population: " + population);
+        System.out.println("Letter: " + letter);
+        System.out.println("Name: " + name);
+
+    }
+}
