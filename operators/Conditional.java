a6476c3e7bc1216df82207cc65aaa7f4c5995b36
Hemavathi23-jm <acharyahema230@gmail.com>
2026-07-08 21:16:59 +0530

    Conditional.java

---
+++ operators/Conditional.java
@@ -0,0 +1,8 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = (a > b) ? a : b; // Conditional operator
+        System.out.println("The greater value is: " + c);//20
+    }
+}
