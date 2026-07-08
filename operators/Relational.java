73d918a23a66d8a0e9193978aec0a1145d11d911
Hemavathi23-jm <acharyahema230@gmail.com>
2026-07-08 20:35:41 +0530

    Relational.java

---
+++ operators/Relational.java
@@ -0,0 +1,14 @@
+public class Relational{
+    public static void main(String [] args){
+        
+        int a=10;
+        int b=20;
+
+        System.out.println("10==20: " + (a==b));//false
+        System.out.println("10!=20: " + (a!=b));//true
+        System.out.println("10>20: " + (a>b));//false
+        System.out.println("10<20: " + (a<b));//true
+        System.out.println("10>=20: " + (a>=b));//false
+        System.out.println("10<=20: " + (a<=b));//true
+    }
+}
