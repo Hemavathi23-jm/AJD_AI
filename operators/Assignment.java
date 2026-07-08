ddc20dd83832077107290cec6b6944c4c203d1b0
Hemavathi23-jm <acharyahema230@gmail.com>
2026-07-08 20:29:52 +0530

    Assignment.java

---
+++ operators/Assignment.java
@@ -0,0 +1,21 @@
+public class Assignment{
+    public static void main(String [] agrs){
+
+        int a=15;
+
+        a+=5;
+        System.out.println("Value of a after addition: " + a);// a=a+5=>20
+        
+        a-=7;
+        System.out.println("Value of a after subtraction: " + a);// a=a-7=>13
+
+        a*=4;
+        System.out.println("Value of a after multiplication: " + a);// a=a*4=>52
+
+        a/=2;
+        System.out.println("Value of a after division: " + a);// a=a/2=>26
+
+        a%=5;
+        System.out.println("Value of a after modulus: " + a);// a=a%5=>1
+    }
+}
