b69dd7357a178de211ad0662eb7765bb0eba184b
Hemavathi23-jm <acharyahema230@gmail.com>
2026-07-08 20:13:43 +0530

    ArithmaticOperator

---
+++ operators/ArithmaticOperator.java
@@ -0,0 +1,15 @@
+public class ArithmaticOperator {
+    public static void main(String[] args) {
+
+        // Variable declaration and initialization
+        int a = 10;
+        int b = 5;
+
+        // Arthmetic operations
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+    }
+}    
